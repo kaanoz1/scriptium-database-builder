dea197beb27b86fb759205e01a3a9aa1b28999d3
Kaan <kaanozarslan@ogr.eskisehir.edu.tr>
2025-11-06 14:13:42 +0300

    namespc Builder (#4)

--- Extractor/Builder/Builder.h
+++ Extractor/Builder/Builder.h
@@ -5,15 +5,17 @@
 #ifndef SCRIPTIUM_DATABASE_BUILDER_BUILDER_H
 #define SCRIPTIUM_DATABASE_BUILDER_BUILDER_H
 
-template <typename TFileInput, typename TOutput>
-class Builder {
+namespace scrptm {
+    template<typename TFileInput, typename TOutput>
+    class Builder {
     public:
         virtual ~Builder() = default;
+
         /*
          @brief This functions build the declared input via travelling along the file it has given.
          */
-        virtual const TOutput& build(TFileInput file) const = 0;
-};
-
+        virtual const TOutput &build(TFileInput file) const = 0;
+    };
+}
 
 #endif //SCRIPTIUM_DATABASE_BUILDER_BUILDER_H
